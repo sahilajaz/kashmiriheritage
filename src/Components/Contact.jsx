c34e95dcdb1d6e6ac38f5639ebbd4de69978361a
sahilajaz <sahilajazw@gmail.com>
2024-08-16 17:40:45 +0530

    contact

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -31,11 +31,11 @@ const Contact = () => {
   };
 
   return (
-    <section className='mt-16 sm:mt-10 w-full lg:mb-80 sm:mb-96 mb-[500px]'>
+    <section className='mt-16 sm:mt-10 w-full lg:mb-96 sm:mb-96 mb-[500px]'>
       <div className="w-full relative">
         <div className='bg-slate-700 w-full h-[30vw] flex flex-col justify-center items-center'>
-          <h1 className='text-center text-sm sm:text-4xl font-extrabold text-white'>Contact Us</h1>
-          <p className='text-center text-sm sm:text-1xl mt-3 text-white'>Questions, feedback—we're here for it all</p>
+          <h1 className='text-center text-sm sm:text-4xl mt-2 sm:mt-0 font-extrabold text-white'>Contact Us</h1>
+          <p className='text-center text-sm sm:text-1xl  mb-8 lg:mb-0 text-white'>Questions, feedback—we're here for it all</p>
         </div>
         <div className="contact-card absolute xl:top-[530px] lg:top-[500px] sm:top-[400px] top-[300px] left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-20 w-full lg:max-w-[800px] max-w-[500px] px-4 sm:px-6 md:px-8">
           <form className='flex flex-col gap-6 bg-white p-6 sm:p-8 rounded-lg shadow-lg' ref={form} onSubmit={sendEmail}>
